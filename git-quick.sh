006283eae73bdc8dc32221dddb87a05ecce46a60
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2024-07-27 16:44:08 +0530

    message

--- git-quick.sh
+++ git-quick.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add .
-message = "committed on $(date) by $(whoami) from $(hostname)"
-echo "commit message = $(message)"
+message="committed on $(date) by $(whoami) from $(hostname)"
+echo "commit message = $message"
 git commit -m message
 git push
