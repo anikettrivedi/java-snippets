5b354a1824d67b7ef46910f7292b21d239439957
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2025-02-03 18:37:56 +0530

    committed on Mon Feb  3 18:37:56 IST 2025 by aniket from Anikets-MacBook-Pro.local

--- src/main/java/multithreading/consumerproducer/ScalableConsumerProducer.java
+++ src/main/java/multithreading/consumerproducer/ScalableConsumerProducer.java
@@ -9,6 +9,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ScalableConsumerProducer {
 
     private static final int CAPACITY = 10;
+
     public static void main(String[] args) throws InterruptedException {
         Semaphore full = new Semaphore(0);
         Semaphore empty = new Semaphore(CAPACITY);
@@ -16,34 +17,34 @@ public class ScalableConsumerProducer {
         Lock lock = new ReentrantLock();
 
         Thread producer = new Thread(() -> {
-           while (true) {
-               try {
-                   Object item = new Object();
-                   empty.acquire();
-                   lock.lock();
-                   System.out.println("Producing new item = " + item);
-                   queue.offer(item);
-                   lock.unlock();
-                   full.release();
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           }
+            while (true) {
+                try {
+                    Object item = new Object();
+                    empty.acquire();
+                    lock.lock();
+                    System.out.println("Producing new item = " + item);
+                    queue.offer(item);
+                    lock.unlock();
+                    full.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
 
         Thread consumer = new Thread(() -> {
-           while (true) {
-               try {
-                   full.acquire();
-                   lock.lock();
-                   Object item = queue.poll();
-                   System.out.println("Consuming item = " + item);
-                   lock.unlock();
-                   empty.release();
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           }
+            while (true) {
+                try {
+                    full.acquire();
+                    lock.lock();
+                    Object item = queue.poll();
+                    System.out.println("Consuming item = " + item);
+                    lock.unlock();
+                    empty.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
 
         producer.start();
