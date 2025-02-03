0a2ebaa9f88a2490d98f2eb87c77671678e8caf6
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2025-02-03 18:31:00 +0530

    committed on Mon Feb  3 18:31:00 IST 2025 by aniket from Anikets-MacBook-Pro.local

---
+++ src/main/java/multithreading/consumerproducer/ScalableConsumerProducer.java
@@ -0,0 +1,54 @@
+package multithreading.consumerproducer;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ScalableConsumerProducer {
+
+    private static final int CAPACITY = 10;
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore full = new Semaphore(0);
+        Semaphore empty = new Semaphore(CAPACITY);
+        Queue<Object> queue = new ArrayDeque<>();
+        Lock lock = new ReentrantLock();
+
+        Thread producer = new Thread(() -> {
+           while (true) {
+               try {
+                   Object item = new Object();
+                   empty.acquire();
+                   lock.lock();
+                   System.out.println("Producing new item = " + item);
+                   queue.offer(item);
+                   lock.unlock();
+                   full.release();
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+        });
+
+        Thread consumer = new Thread(() -> {
+           while (true) {
+               try {
+                   full.acquire();
+                   lock.lock();
+                   Object item = queue.poll();
+                   System.out.println("Consuming item = " + item);
+                   lock.unlock();
+                   empty.release();
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+        });
+
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+    }
+}
---
+++ src/main/java/multithreading/consumerproducer/SingleConsumerProducer.java
@@ -0,0 +1,43 @@
+package multithreading.consumerproducer;
+
+import java.util.concurrent.Semaphore;
+
+public class SingleConsumerProducer {
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore full = new Semaphore(0);
+        Semaphore empty = new Semaphore(1);
+        Object[] object = new Object[1];
+
+        Thread producer = new Thread(() -> {
+            while (true) {
+                try {
+                    empty.acquire();
+                    object[0] = new Object();
+                    System.out.println("producing new object = " + object[0]);
+                    full.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                try {
+                    full.acquire();
+                    System.out.println("consuming object = " + object[0]);
+                    object[0] = null;
+                    empty.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+
+    }
+}
