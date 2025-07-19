23298c6e63de83c01dde657741c924641e9b1ec4
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2025-07-19 22:54:43 +0530

    committed on Sat Jul 19 22:54:43 IST 2025 by aniket from Anikets-MacBook-Pro.local

---
+++ src/main/java/multithreading/volatilekeyword/WithVolatile.java
@@ -0,0 +1,35 @@
+package multithreading.volatilekeyword;
+
+public class WithVolatile {
+
+    public static final int NO_OF_ITERATIONS = 100_000;
+
+    private static volatile int counterOne = 0;
+    private static volatile int counterTwo = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread incrementingThreadOne = new Thread(() -> {
+            for (int i = 0; i < NO_OF_ITERATIONS; i++) {
+                counterOne++;
+                counterTwo++;
+            }
+        });
+
+        Thread checkerThread = new Thread(() -> {
+            for (int i = 0; i < NO_OF_ITERATIONS; i++) {
+                if (counterTwo > counterOne) {
+                    System.out.println("CounterTwo > CounterOne !!");
+                }
+            }
+        });
+
+        incrementingThreadOne.start();
+        checkerThread.start();
+
+        incrementingThreadOne.join();
+        checkerThread.join();
+
+    }
+
+}
